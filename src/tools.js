c1414af63d2d843d81603f0f39e976ad84f6f4d3
HonzaSoukup <jendasou@gmail.com>
2023-08-20 09:32:05 +0200

    Update tools.js

--- src/tools.js
+++ src/tools.js
@@ -146,7 +146,7 @@ export async function extractProperties({ page, dataset }) {
         const output = [];
         [...document.querySelectorAll('.dir-property-list > .property')].map((listing) => {
             if (!listing.querySelector('span[class*=tip]')) {
-                output.push({ url: listing.querySelector('a').href });
+                output.push({ url: listing.querySelector('a').href,  name: listing.querySelector('.name').text, locality: listing.querySelector('.locality').text,price: listing.querySelector('.prices').text  });
             }
         });
         return output;
